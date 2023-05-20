59ad193fa3c561e06929ac48290c53cbe24d1a46
vaishureddy1919 <vaishureddypappireddy@gmail.com>
2023-05-20 13:17:46 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def srujana(request):
+    return HttpResponse('<h1>hiii hello</h1>')
+
+def ravi(reruest):
+    return HttpResponse('<buttn>hii i am ravi</buttn>')
+
